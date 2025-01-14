4f59eea5bc3683e3ee5a5f5ddb51971a61e5a6b4
James Zhong <thebitspud@gmail.com>
2025-01-14 09:34:32 -0800

    Implement backend testing scaffold

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,14 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettierConfig from "eslint-config-prettier";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tsEslint from 'typescript-eslint'
+import eslintJest from 'eslint-plugin-jest'
+import prettierConfig from 'eslint-config-prettier'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser, parser: tseslint.parser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser, parser: tsEslint.parser } },
   pluginJs.configs.recommended,
-  tseslint.configs.eslintRecommended,
+  tsEslint.configs.eslintRecommended,
+  eslintJest.configs.recommended,
   prettierConfig,
-];
+]
---
+++ backend/tests/user.test.ts
@@ -0,0 +1,9 @@
+import { app } from '../src/app'
+
+describe('User Registration', () => {
+  test('registers a user', () => {
+    const a = app
+
+    expect(1 + 2).toBe(3)
+  })
+})
