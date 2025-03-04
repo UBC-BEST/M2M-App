eb605e0ce76624b781a3d6b39686f0900fae0c2f
kellydiao <kellydiao07@gmail.com>
2025-03-03 21:10:05 -0800

    Added 3 buttons in settings page to email, name, and password page

---
+++ frontend/lib/pages/Home/name_setting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class NameSettingPage extends StatelessWidget {
+  const NameSettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name Settings'),
+      ),
+    );
+  }
+}
--- frontend/lib/pages/Home/settings_page.dart
+++ frontend/lib/pages/Home/settings_page.dart
@@ -1,23 +1,56 @@
-// pages/settings_page.dart
 import 'package:flutter/material.dart';
+import 'name_setting_page.dart';
+import 'email_setting_page.dart';
+import 'password_setting_page.dart';
+
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Card(
-      shadowColor: Colors.transparent,
-      margin: const EdgeInsets.all(0),
-      child: SizedBox.expand(
-        child: Center(
-          child: Text(
-            'Display Settings Here',
-            style: theme.textTheme.titleLarge,
-          ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Header
+            //TPrimaryHeaderContainer(child: child),
+            //Do I need a header? I saw this in a Youtube tutorial so...
+
+
+            // name_setting_page(): own page for name changes, button navigates here
+            // email_setting_page(): own page for email changes
+            // password_setting_page(): own page for password changes
+            _settingsButton(context, "Name", NameSettingPage()),
+            _settingsButton(context, "Email", EmailSettingPage()),
+            _settingsButton(context, "Password", PasswordSettingPage()),
+          ],
         ),
       ),
+       
     );
   }
+}
+
+
+
+
+// _settingsButton: button that navigates to defined screen when pressed
+Widget _settingsButton(BuildContext context, String title, Widget screen) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 15), //haven't put in actual dimensions yet
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => screen),
+        );
+      },
+      child: Text(title),
+    ),
+  );
 }
